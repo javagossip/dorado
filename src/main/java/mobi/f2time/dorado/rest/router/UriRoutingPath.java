561e079572948997d7d8963667e33dd5d8df15c2
weiping wang <javagossip@gmail.com>
2018-08-01 13:38:44 +0800

    format code

--- src/main/java/mobi/f2time/dorado/rest/router/UriRoutingPath.java
+++ src/main/java/mobi/f2time/dorado/rest/router/UriRoutingPath.java
@@ -15,9 +15,7 @@
  */
 package mobi.f2time.dorado.rest.router;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
