8675196115601c75cb1665ed9e86bcd7c5474b6a
weipingwang <javagossip@gmail.com>
2025-03-25 18:40:39 +0800

    fix: 采用内置的maven wrapper命令替换mvn

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "prepare dorado release..."
-mvn release:prepare -Prelease
+./mvnw release:prepare -Prelease
 echo "perform dorado release..."
-mvn release:perform -Prelease
+./mvnw release:perform -Prelease
 
 
